9ebc01a8733ce780af9c14094707288862d10715
SHAN-SN <85439835+SHAN-SN@users.noreply.github.com>
2022-07-17 12:17:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -62,8 +62,7 @@ async def start(client, message):
             ]
         ]
 
-        if message.command[1] != "subscribe":
-            try:
+        if message.command[1] != "subscribed
             	btn.append([InlineKeyboardButton("Hᴇʏ Bᴏᴛ....! Wʜʏ I'ᴍ ᴊᴏɪɴɪɴɢ", callback_data="neosub")])
         await client.send_message(
             chat_id=message.from_user.id,
